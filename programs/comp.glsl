f0d801e4f52deeec33d10955dbd448bfedfc6314
Nabil Mansour <nabilmansour1999@gmail.com>
2022-12-20 14:48:25 -0500

    corrected link to qualifier type inside the compute shader

--- programs/comp.glsl
+++ programs/comp.glsl
@@ -2,7 +2,7 @@
 
 layout (local_size_x = 16, local_size_y = 16, local_size_z = 1) in;
 
-// refer to this https://www.khronos.org/opengl/wiki/Data_Type_(GLSL)#Opaque_types
+// refer to this https://www.khronos.org/opengl/wiki/Layout_Qualifier_(GLSL)
 layout(rgba32f, location=0) uniform image2D texture_buffer;
 
 /*
